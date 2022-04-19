39361bded950883462407561429236acc5c1212b
Laurajcb <callejaslaura96@gmail.com>
2022-04-19 10:36:54 -0500

    tryin a new fix

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,7 +3,7 @@ export default function updateStudentGradeByCity(students, city, newGrade) {
     .filter((student) => student.location == city)
     .map((student) => {
       let grades = newGrade.find((grade) => grade.studentId == student.id);
-      let grade = grades ? grades.grade : "N/A";
+      let grade = grades ? grades.grade : 'N/A';
       return { ...student, grade };
     });
 }
